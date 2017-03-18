335847e67d3584de30dde2ae75f07c98d7098c38
严俊东 <1172482914@qq.com>
2017-03-18 10:47:46 +0800

    简单的代理

---
+++ 01简单的代理/proxy01.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 严俊东 on 2017/2/25.
+ */
+"use strict";
+const express = require('express');
+const path = require('path');
+const config = require('./config.json');
+var proxy = require('http-proxy-middleware');
+const app = express();
+
+for (let key in config.map) {
+  console.log(key)
+  app.use(key, proxy({target: config.map[key], changeOrigin: true}));
+}
+
+
+app.listen(config.port, '127.0.0.1', function () {
+  console.log('http://' + config.localhost + ":" + config.port);
+});
