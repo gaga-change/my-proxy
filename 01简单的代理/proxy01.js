f167f7ecaa693535a9c261b2abaf3420252aa81b
严俊东 <1172482914@qq.com>
2017-11-06 15:43:10 +0800

    静态目录

--- 01简单的代理/proxy01.js
+++ 01简单的代理/proxy01.js
@@ -13,6 +13,7 @@ for (let key in config.map) {
   app.use(key, proxy({target: config.map[key], changeOrigin: true}));
 }
 
+// app.use('/', express.static('./static')) // 静态目录
 
 app.listen(config.port, '127.0.0.1', function () {
   console.log('http://' + config.localhost + ":" + config.port);
